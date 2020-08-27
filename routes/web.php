d6952bc1ce94f83e80b3c287e98a1de7e1b07433
Marcio Camello <marciocamello@outlook.com>
2020-08-27 13:00:23 -0300

    Add auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'WelcomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
