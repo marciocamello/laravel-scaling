ae842fd7714aa73528df5fdbb09893527547d600
Marcio Camello <marciocamello@outlook.com>
2020-08-27 11:24:45 -0300

    Deploy

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-composer install
+echo "🎬 entrypoint.sh"
+
+composer dump-autoload --no-interaction --no-dev --optimize
+
+echo "🎬 artisan commands"
+
 php artisan cache:clear
 
 #Production
 #php artisan migrate --no-interaction --force
 
+echo "🎬 rewrite permissions"
+
 chown -R www-data:www-data /home/storage
 chown -R www-data:www-data /home/bootstrap/cache
+
+echo "🎬 start supervisord"
 
 supervisord -c /home/process/supervisord.conf -n
